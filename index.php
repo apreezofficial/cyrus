409c21838415861c2983c1e25369faf31a268191
apreezofficial <apreezofficial@gmail.com>
2025-06-10 23:22:17 +0100

    Uodated footer page

--- index.php
+++ index.php
@@ -886,23 +886,8 @@
     <!-- Bottom Bar -->
     <div class="mt-16 pt-8 border-t border-gray-700 flex flex-col md:flex-row justify-between items-center">
       <p class="text-gray-400 text-sm">
-        &copy; <?php echo ('Y') ?> Cyrus Tech. All rights reserved.
+        &copy; <?php echo date('Y') ?> Cyrus Tech. All rights reserved.
       </p>
-      
-      <div class="mt-4 md:mt-0 flex items-center space-x-6">
-        <!-- Theme Toggle -->
-        <button id="theme-toggle-footer" type="button" class="text-gray-400 hover:text-white">
-          <svg id="theme-toggle-dark-icon-footer" class="w-5 h-5" fill="currentColor" viewBox="0 0 20 20">
-            <path d="M17.293 13.293A8 8 0 016.707 2.707a8.001 8.001 0 1010.586 10.586z"></path>
-          </svg>
-          <svg id="theme-toggle-light-icon-footer" class="w-5 h-5 hidden" fill="currentColor" viewBox="0 0 20 20">
-            <path d="M10 2a1 1 0 011 1v1a1 1 0 11-2 0V3a1 1 0 011-1zm4 8a4 4 0 11-8 0 4 4 0 018 0zm-.464 4.95l.707.707a1 1 0 001.414-1.414l-.707-.707a1 1 0 00-1.414 1.414zm2.12-10.607a1 1 0 010 1.414l-.706.707a1 1 0 11-1.414-1.414l.707-.707a1 1 0 011.414 0zM17 11a1 1 0 100-2h-1a1 1 0 100 2h1zm-7 4a1 1 0 011 1v1a1 1 0 11-2 0v-1a1 1 0 011-1zM5.05 6.464A1 1 0 106.465 5.05l-.708-.707a1 1 0 00-1.414 1.414l.707.707zm1.414 8.486l-.707.707a1 1 0 01-1.414-1.414l.707-.707a1 1 0 011.414 1.414zM4 11a1 1 0 100-2H3a1 1 0 000 2h1z" clip-rule="evenodd"></path>
-          </svg>
-        </button>
-        
-        <a href="#" class="text-gray-400 hover:text-white text-sm">Accessibility</a>
-        <a href="#" class="text-gray-400 hover:text-white text-sm">Sitemap</a>
-      </div>
     </div>
   </div>
 </footer>
